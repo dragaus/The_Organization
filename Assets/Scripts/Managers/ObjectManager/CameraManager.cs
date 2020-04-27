8daea68a479b0be869cb508889a4a0bd00d206a3
Francisco Castaneda <dragalichi@gmail.com>
2020-04-26 19:57:25 -0500

    Clamp the camera

--- Assets/Scripts/Managers/ObjectManager/CameraManager.cs
+++ Assets/Scripts/Managers/ObjectManager/CameraManager.cs
@@ -22,12 +22,14 @@ public class CameraManager : MonoBehaviour
     {
         var newPos = transform.position;
         newPos.x += Time.deltaTime * speed * Input.GetAxis("Horizontal");
+        newPos.x = Mathf.Clamp(newPos.x, limits[0].minLimit, limits[0].maxLimit);
 
         var fieldOfView = cam.fieldOfView + (Time.deltaTime * zoomSpeed * Input.mouseScrollDelta.y);
         fieldOfView = Mathf.Clamp(fieldOfView, limits[1].minLimit, limits[1].maxLimit);
         cam.fieldOfView = fieldOfView;
 
         newPos.z += Time.deltaTime * speed * Input.GetAxis("Vertical");
+        newPos.z = Mathf.Clamp(newPos.z, limits[2].minLimit, limits[2].maxLimit);
         transform.position = newPos;
     }
 }
